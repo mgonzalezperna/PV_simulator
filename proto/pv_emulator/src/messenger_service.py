ba558d340012778e13302b1bad3f4982390da513
mgonzalezperna <martin@skaro.localdomain>
2019-10-05 01:51:45 -0300

    Build new package, create a alpha version of meter.

---
+++ proto/pv_emulator/src/messenger_service.py
@@ -0,0 +1,53 @@
+import pika
+
+
+class MessengerService:
+    '# This service provides an interface to send messages to any consumer.'
+    broker_hostname = 'localhost'
+    broker_port = 5672
+    broker_user = 'guest'
+    broker_passwd = 'guest'
+    global_connection = None
+    channel = None
+
+    def create_connection(self):
+        '''Creates a new global_connection with a message broker'''
+        credentials = pika.PlainCredentials(
+                self.broker_user, self.broker_passwd)
+        parameters = pika.ConnectionParameters(
+                self.broker_hostname, self.broker_port, '/', credentials)
+        connection = pika.BlockingConnection(parameters)
+        return connection
+
+    def set_channel(self, connection, queue_name):
+        '''Setups the broker queue and returns the channel'''
+        channel = connection.channel()
+        channel.queue_declare(queue=queue_name)
+        return channel
+
+    def send_measurement(self, measurement):
+        '''Method to send measurement through channel.'''
+        self.channel.basic_publish(
+                exchange='', routing_key=self.queue_name, body=measurement)
+        print("[x] Sent message")
+
+    def consume_measurements(self, callback):
+        '''Method to listen to measurement through channel.
+        The callback parameter must be a function with an interface like > 
+        def callback(ch, method, properties, body)'''
+        self.channel.basic_consume(
+            queue=self.queue_name, auto_ack=True, on_message_callback=callback)
+        self.channel.start_consuming()
+
+    def close_connection():
+        '''Method to close gracefully the global_connection with broker'''
+        global global_connection
+        global_connection.close()
+
+    def __init__(self, queue_name):
+        self.queue_name = queue_name
+        self.global_connection = self.create_connection()
+        self.channel = self.set_channel(self.global_connection, queue_name)
+
+    if __name__ == "__main__":
+        __init__()
---
+++ proto/pv_emulator/tests/test_pv_emulator.py
@@ -0,0 +1,5 @@
+from pv_emulator import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
